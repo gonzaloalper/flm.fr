46c2cdf022c45faa4418a2156b526114a4fb474a
Gonzalo Álvarez Pérez <38098347+gonzaloalper@users.noreply.github.com>
2018-04-12 20:49:37 +0200

    Update fregre.lr.pc.R

--- fregre.lr.pc.R
+++ fregre.lr.pc.R
@@ -10,7 +10,7 @@ fregre.lr.pc=function (fdataobj, Y, ncomp = 3)
   
   # Linear regression using multivariate model and pseudo-inverse
   hat_beta <- t(pcX$x[,1:x_col])%*%pcX$x[,1:x_col]
-  hat_beta <- Minverse(hat_beta)
+  hat_beta <- pseudoinverse(hat_beta)
   hat_beta <- hat_beta%*%t(pcX$x[,1:x_col])
   hat_beta <- hat_beta%*%pcY$x[,1:y_col]
 
