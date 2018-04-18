49a2248e04dcc852826e4853d236d14bbda94f12
Gonzalo Álvarez Pérez <38098347+gonzaloalper@users.noreply.github.com>
2018-04-19 00:00:16 +0200

    Update fregre.fr.pc.R

--- R/fregre.fr.pc.R
+++ R/fregre.fr.pc.R
@@ -1,12 +1,13 @@
-fregre.lr.pc=function (fdataobj, Y, ncomp = 3) 
+fregre.fr.pc=function (X, Y, k_x = 3, k_y = 3) 
+  # Returns the regression parameters for a functional respone Y and a functional covariate X,
+  # computed in two principal components bases, with k_y and k_x elements respectively.
 {
   # Compute principal components
-  pcX <- flm.pc(fdataobj)
-  pcY <- flm.pc(Y)
+  pcX <- flm.pc(X, k_x) # k_x principal components of X
+  pcY <- flm.pc(Y, k_y) # k_y principal components of Y
   
   # Dimensions
-  x_row = nrow(pcX$x); x_col = ncol(pcX$x)
-  y_row = nrow(pcY$x); x_col = ncol(pcY$x)
+  x_col = ncol(pcX$x); y_col = ncol(pcY$x)
   
   # Linear regression using multivariate model and pseudo-inverse
   hat_beta <- t(pcX$x[,1:x_col])%*%pcX$x[,1:x_col]
