1b00109886d63babcbf566cc83f1bf4921163842
Gonzalo Álvarez Pérez <38098347+gonzaloalper@users.noreply.github.com>
2018-04-23 17:21:03 +0200

    Update fregre.fr.pc.ex.R

--- fregre.fr.pc.ex.R
+++ fregre.fr.pc.ex.R
@@ -11,9 +11,10 @@ sourceDir <- function(directory = "R", ...) {
 sourceDir()
 
 # Argvals
-l <- 201
-ss <- seq(0, 1, l = l) # Argvals of X
-tt <- ss # Argvals of Y
+lx <- 11
+ly <- 101
+ss <- seq(0, 1, l = lx) # Argvals of X
+tt <- seq(0, 1, l = ly) # Argvals of Y
 
 # Theoretical beta
 beta <- function(s, t) {
@@ -24,16 +25,16 @@ beta <- function(s, t) {
 # Visualization
 image(ss, tt, outer(ss, tt, FUN = beta), col = viridis(20))
 
-# Sample covatiate
+# Sample covariate
 library(fda.usc)
-n <- 1000
-fdataobj <- r.ou(n = n, x0 = seq(-5, 5, l = n), alpha = 2)
+n <- 20
+fdataobj <- r.ou(n = n, x0 = seq(-5, 5, l = n), alpha = 2, t = ss)
 
 # Error
-noise <- matrix(rnorm(n = n * l, mean = 0, sd = 0.1), nrow = n, ncol = l)
+noise <- matrix(rnorm(n = n * ly, mean = 0, sd = 0.1), nrow = n, ncol = ly)
 
 # Response: Y(t) = \int X(s) beta(s, t) ds + eps(t)
-Y <- fdata(mdata = ss, argvals = ss)
+Y <- fdata(mdata = noise, argvals = tt)
 Y$data <- fdataobj$data %*% outer(ss, tt, beta) + noise
 
 # Plots
@@ -41,33 +42,35 @@ par(mfrow = c(1, 2))
 plot(fdataobj)
 plot(Y)
 
-npc<-5
-pcX <- fpc(fdataobj,npc)
-pcY <- fpc(Y,npc)
+npcX<-5
+npcY<-3
+pcX <- fpc(fdataobj,npcX)
+pcY <- fpc(Y,npcY)
 
-hat_beta <- t(pcX$x[,1:npc])%*%pcX$x[,1:npc]
-hat_beta <- fda.usc:::Minverse(hat_beta)
-hat_beta <- hat_beta%*%t(pcX$x[,1:npc])
-hat_beta <- hat_beta%*%pcY$x[,1:npc]
+hat_beta <- t(pcX$x[,1:npcX])%*%pcX$x[,1:npcX]
+hat_beta <- pseudoinverse(hat_beta)
+hat_beta <- hat_beta%*%t(pcX$x[,1:npcX])
+hat_beta <- hat_beta%*%pcY$x[,1:npcY]
 
 print(hat_beta)
 
-surface_beta<-0
-for (i in 1:npc){
-  for (j in 1:npc){
+surface_beta_hat <- 0
+for (i in 1:npcX){
+  for (j in 1:npcY){
     acc<-hat_beta[i,j]*outer(pcX$rotation$data[i,],pcY$rotation$data[j,])
-    surface_beta<-surface_beta+acc
+    surface_beta_hat<-surface_beta_hat+acc
   }
 }
 
 image(surface_beta,col = viridis(20))
-image(ss, tt, outer(ss, tt, FUN = beta), col = viridis(20))
+image(ss, tt, surface_beta<-outer(ss, tt, FUN = beta), col = viridis(20))
+max(surface_beta_hat-surface_beta)
 
-theoretical_beta <- matrix(0, nrow = npc, ncol = npc)
+theoretical_beta <- matrix(0, nrow = npcX, ncol = npcY)
 acc_aux<-outer(ss, tt, FUN = beta)
-for (i in 1:npc){
-  for (j in 1:npc){
-    acc<-acc_aux%*%outer(pcX$rotation$data[i,],pcY$rotation$data[j,])
+for (i in 1:npcX){
+  for (j in 1:npcY){
+    acc<-acc_aux*outer(pcX$rotation$data[i,],pcY$rotation$data[j,])
     theoretical_beta[i,j]<-sdetorus::integrateSimp2D(acc,c(1,1))
   }
 }
