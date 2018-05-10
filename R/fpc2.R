3bdfc6934837d38246668a280c9ef27d3c98acf1
Gonzalo Álvarez Pérez <38098347+gonzaloalper@users.noreply.github.com>
2018-05-11 00:00:00 +0200

    Update fpc2.R

--- R/fpc2.R
+++ R/fpc2.R
@@ -1,6 +1,5 @@
-fpc2 = function (fdataobj, ncomp = 3) 
+fpc2 = function (fdataobj, ncomp = 3, equispaced) 
 {
-  #browser()
   X <- fdataobj[["data"]]
   tt <- fdataobj[["argvals"]]
   rtt <- fdataobj[["rangeval"]]
@@ -16,13 +15,27 @@ fpc2 = function (fdataobj, ncomp = 3)
   # SVD way
   eigenres <- svd(X_cen)
   v <- eigenres$v
-  # u <- eigenres$u # No necesario?
   d <- eigenres$d
+  #if(missing(equispaced)){
+  #  #comprobar si lo son
+  #  sep <- diff(X$argvals)
+  #  equi <- all(sep[1] == sep[-1])
+  #} 
   
-  # Scores al estilo de componentes principales
-  scores1 <- X_cen %*% v[, l]
+  #else if(equispaced == TRUE){
+    h <- (rtt[2] - rtt[1])/(length(tt) - 1)
+    v <- v/sqrt(h) # functional normalization
+    # Scores al estilo de componentes principales
+    scores1 <- h * X_cen %*% v[, l]
+  #}
   
-  out <- list(rotation = v[, l], x = scores1, mean = x_mean, l = l)
+  #else if(equispaced == FALSE){
+    #v <- v/sqrt(h) # functional normalization
+    # Scores al estilo de componentes principales
+    #scores1 <- h * X_cen %*% v[, l]
+  #}
+  
+  out <- list(rotation = v[, l], x = scores1, mean = x_mean, l = l, h = h)
   class(out) = "fdata.comp"
   return(out)
 }
