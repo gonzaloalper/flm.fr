71aa6c4941a7dfbf25bd31e1f13c909a08afa545
Gonzalo Álvarez Pérez <38098347+gonzaloalper@users.noreply.github.com>
2018-07-24 21:54:39 +0200

    Update fregre.fr.pc.ex.R

--- fregre.fr.pc.ex.R
+++ fregre.fr.pc.ex.R
@@ -2,7 +2,7 @@
 
 library(viridis)
 
-# setwd("C:/Users/gonza/Desktop")
+setwd("C:/Users/gonza/Desktop")
 # Execute all the functions in the R directory from the above file:
 sourceDir <- function(directory = "R", ...) {
   invisible(sapply(dir(directory),
@@ -11,16 +11,18 @@ sourceDir <- function(directory = "R", ...) {
 sourceDir()
 
 # Argvals
-lx <- 201
+lx <- 101
 ly <- 101
-ss <- seq(0, 50, l = lx) # Argvals of X
+ss <- seq(3, 4, l = lx) # Argvals of X
 tt <- seq(1, 2, l = ly) # Argvals of Y
 
 # Theoretical beta
 beta <- function(s, t) {
+  #diag(2,lx,ly)
+  #2+(s - s + t - t)
   #5 / (1 + (s - 0.5)^2) * cos(2 * pi * t * s) / 200
-  #(s + t^2) / 100
-  cos(t * s / pi)^2
+  (s + t^2) / 100
+  #cos(t * s / pi)^2
 }
 
 # Visualization
@@ -29,29 +31,44 @@ surface_beta <- outer(ss, tt, FUN = beta)
 # Sample covariate
 library(fda.usc)
 n <- 200
-X <- r.ou(n = n, x0 = seq(-10, 10, l = n), alpha = 2, t = ss)
+X <- rproc2fdata(n = n, ss, sigma="brownian")
+#X <- r.ou(n = n, x0 = seq(-10, 10, l = n), alpha = 2, t = ss)
+X_range <- X$rangeval[2] - X$rangeval[1]
 
 # Error
-noise <- matrix(rnorm(n = n * ly, mean = 0, sd = 0.1), nrow = n, ncol = ly)
+#noise <- matrix(0, nrow = n, ncol = ly)
+noise <- matrix(rnorm(n = n * ly, mean = 0, sd = 0), nrow = n, ncol = ly)
 
 # Response: Y(t) = \int X(s) beta(s, t) ds + eps(t)
 Y <- fdata(mdata = noise, argvals = tt)
-Y$data <- X$data %*% surface_beta + noise
+
+#Y$data <- X$data %*% surface_beta * hx
+
+for (j in 1:ly){
+  b = surface_beta[,j]
+  sapply(1:n, function(i){
+    Y$data[i,j] <<- sdetorus::integrateSimp1D(b * X$data[i,], X_range)
+  })
+}
+
+Y$data <- Y$data + noise 
+
+Y_range <- Y$rangeval[2] - Y$rangeval[1]
 
 # Plots
 par(mfrow = c(1, 2))
 plot(X)
 plot(Y)
 
-npcX<-200
-npcY<-100
+npcX<-10
+npcY<-10
 pcX <- fpc2(X,npcX,equispaced = TRUE)
 pcY <- fpc2(Y,npcY,equispaced = TRUE)
 
 hat_b <- t(pcX$x[,1:npcX])%*%pcX$x[,1:npcX]
 hat_b <- pseudoinverse(hat_b)
 hat_b <- hat_b%*%t(pcX$x[,1:npcX])
-hat_b <- hat_b%*%pcY$x[,1:npcY]*sqrt(pcX$h)*sqrt(pcY$h)
+hat_b <- hat_b%*%pcY$x[,1:npcY]
 
 print(hat_b)
 
@@ -62,16 +79,18 @@ print(hat_b)
 surface_beta_hat <- matrix(0, nrow = lx, ncol = ly)
 for (i in 1:npcX){
   for (j in 1:npcY){
-    #acc<-hat_beta[i,j]*outer(pcX$rotation[,i], pcY$rotation[,j])*sqrt(pcX$h/pcY$h)
-    acc <- hat_b[i,j]*pcX$rotation[,i]%*%t(pcY$rotation[,j])*sqrt(pcX$h/pcY$h)
+    acc<-hat_b[i,j]*outer(pcX$rotation[,i], pcY$rotation[,j])
+    #acc <- hat_b[i,j]*pcX$rotation[,i]%*%t(pcY$rotation[,j])
     surface_beta_hat <- surface_beta_hat+acc
     # surface_beta <- outer(ss, tt, FUN = beta)
   }
 }
 
-limites = c(min(surface_beta_hat), max(surface_beta_hat))
-image(ss, tt, surface_beta_hat,col = viridis(20))
-image(ss, tt, surface_beta, col = viridis(20), zlim = limites)
+minima <- c(min(surface_beta_hat), min(surface_beta))
+maxima <- c(max(surface_beta_hat), max(surface_beta))
+limits = c(min(minima), max(maxima))
+image(ss, tt, surface_beta_hat, col = viridis(20), zlim = limits)
+image(ss, tt, surface_beta, col = viridis(20), zlim = limits)
 
 #persp(fdataobj$argvals, Y$argvals, surface_beta_hat, phi = 0, theta = 60)
 #persp(ss, tt, surface_beta, phi = 0, theta = 60)
@@ -82,9 +101,17 @@ theoretical_beta <- matrix(0, nrow = npcX, ncol = npcY)
 acc_aux<-outer(ss, tt, FUN = beta)
 for (i in 1:npcX){
   for (j in 1:npcY){
-    acc<-acc_aux*outer(pcX$rotation[,i],pcY$rotation[,j])*sqrt(pcY$h/pcX$h)
-    theoretical_beta[i,j]<-sum(acc)*(pcX$h)*(pcY$h)
+    acc<-acc_aux*outer(pcX$rotation[,i],pcY$rotation[,j])
+    theoretical_beta[i,j]<-sum(acc)*pcX$h*pcY$h
     #theoretical_beta[i,j]<-sdetorus::integrateSimp2D(acc,c(1,1))
+  }
+}
+
+t_beta <- matrix(0, nrow = npcX, ncol = npcY)
+for (i in 1:npcX){
+  for (j in 1:npcY){
+    feval <- acc_aux[i,j] * 2 * outer(sin((i - 0.5) * pi * ss),sin((j - 0.5) * pi * tt))
+    t_beta[i,i] <- sdetorus::integrateSimp2D(feval,c(1,1))
   }
 }
 
@@ -94,7 +121,4 @@ diferencia
 explained_variance_X <- sum(pcX$d[1:npcX])/sum(pcX$d); explained_variance_X
 explained_variance_Y <- sum(pcY$d[1:npcY])/sum(pcY$d); explained_variance_Y
 
-hat_y <- pcX$x%*%hat_b
-residuals <- abs(hat_y - pcY$x)
-
-PCvM_statistic(residuals)
+pvalue <- flm_test(10000, n, X, pcX, pcY, hat_b)
