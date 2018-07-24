220322e032bb32e5338141b5903f7d260e74471d
Eduardo García-Portugués <egarpor@users.noreply.github.com>
2018-07-25 00:13:23 +0200

    Fix multiple bugs

--- R/linear_model.R
+++ R/linear_model.R
@@ -1,24 +1,29 @@
-linear_model = function(X, n, ly, argvals_y, std_dev, surface_beta)
+linear_model <- function(X, argvals_y, std_dev, beta)
 {
+  
+  # Sample size
+  n <- length(X)
+  ly <- length(argvals_y)
+  
   # Error
   noise <- matrix(rnorm(n = n * ly, mean = 0, sd = std_dev), nrow = n, ncol = ly)
   
+  # Store surface
+  surface_beta <- outer(X$argvals, argvals_y, beta)
+  
   # Response: Y(t) = \int X(s) beta(s, t) ds + eps(t)
-  obj <- fdata(mdata = noise, argvals = argvals_y)
-  
-  #Y$data <- X$data %*% surface_beta * hx
-  
-  for (j in 1:ly){
-    b = surface_beta[,j]
-    sapply(1:n, function(i){
-    obj[i,j] <<- sdetorus::integrateSimp1D(b * X$data[i,], X_range)
+  Y <- fdata(mdata = noise, argvals = argvals_y, rangeval = range(argvals_y), 
+             names = list(main = "Functional response", ylab = "Y"))
+  length_x <- diff(X$rangeval)
+  for (j in 1:ly) {
+    b <- surface_beta[, j]
+    Y$data[, j] <- sapply(1:n, function(i) {
+      sdetorus::integrateSimp1D(b * X$data[i, ], length_x)
     })
   }
   
+  # Add noise
   Y$data <- Y$data + noise 
-  rangevals_y <- c(tt[1], tail(tt, n=1))
-  
-  Y = fdata(data = obj$data, argvals = tt, rangeval = rangevals_y, 
-            names = list(main = "Functional response", ylab = Y))
   return(Y)
+  
 }
