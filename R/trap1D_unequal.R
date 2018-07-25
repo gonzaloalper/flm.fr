95a11c3cf252361d9f5f670b48917d77efdf8334
Gonzalo Álvarez Pérez <38098347+gonzaloalper@users.noreply.github.com>
2018-07-25 15:48:38 +0200

    Update trap1D_unequal.R

--- R/trap1D_unequal.R
+++ R/trap1D_unequal.R
@@ -3,6 +3,6 @@ trap1D_unequal <- function (argvals, fx, na.rm = TRUE)
   lengthInterval <- tail(argvals, n = 1) - argvals[1]
   n <- sum(!is.na(fx))
   h <- diff(argvals)
-  int <-   h[2:length(h)] * 0.5 * (fx[1:length(h)-1] - fx[2:length(h)])
+  int <-   sum(h[2:length(h)] * 0.5 * (fx[2:length(h)] + fx[1:length(h)-1]))
   return(int)
 }
