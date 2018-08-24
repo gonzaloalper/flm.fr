a754f6e6ff3ed18399f68871e85a2d43f5c52ad3
Gonzalo Álvarez Pérez <38098347+gonzaloalper@users.noreply.github.com>
2018-08-24 12:44:03 +0200

    Update fregre.fr.pc.ex.R

--- fregre.fr.pc.ex.R
+++ fregre.fr.pc.ex.R
@@ -3,20 +3,20 @@ fregre.pc.ex <- function(B)
   library(fda.usc)
   library(viridis)
   # Execute all the functions in the R directory from the above file:
-  sourceDir <- function(directory = "R", ...) {
-    invisible(sapply(dir(directory),
-                     function(x) source(paste0(directory, "/", x), ...)))
-  }
-  sourceDir()
+  #sourceDir <- function(directory = "R", ...) {
+  #  invisible(sapply(dir(directory),
+  #                   function(x) source(paste0(directory, "/", x), ...)))
+  #}
+  #sourceDir()
   
   # Argvals
   lx <- 205
   ly <- 205
-  ss <- seq(0, 3, l = lx) # Argvals of X
-  tt <- seq(0, 2, l = ly) # Argvals of Y
+  ss <- seq(0, 1, l = lx) # Argvals of X
+  tt <- seq(0, 1, l = ly) # Argvals of Y
   
   # Sample covariate
-  n <- 200
+  n <- 100
   X <- rproc2fdata(n = n, ss, sigma="brownian")
   #X <- r.ou(n = n, x0 = seq(-10, 10, l = n), alpha = 2, t = ss)
   X_range <- X$rangeval[2] - X$rangeval[1]
@@ -26,8 +26,8 @@ fregre.pc.ex <- function(B)
   beta <- function(s, t) {
     #diag(2,lx,ly)
     #2+(s - s + t - t)
-    500 / (1 + (s - 0.5)^2) * cos(2 * pi * t * s) / 200
-    #(s + t^2) / 100
+    #cos(2 * pi * t * s) / (1 + (s - 0.5)^2) 
+    pi^2*(s^2-1/(0.25+t)) * tanh(1+s + t^3)
     #cos(t * s / pi)^2
   }
   
@@ -43,7 +43,7 @@ fregre.pc.ex <- function(B)
   plot(X)
   plot(Y)
   
-  npcX<-31
+  npcX<-25
   npcY<-25
   pcX <- fpc(X,npcX,equispaced = TRUE)
   pcY <- fpc(Y,npcY,equispaced = TRUE)
@@ -63,7 +63,11 @@ fregre.pc.ex <- function(B)
     }
   }
   
-  #====================================================================
+  #EXPLAINED VARIANCE
+  
+  diferencia <- (sum(((hat_b - theoretical_beta)/theoretical_beta)^2))^(1/(npcX*npcY)); 
+  explained_variance_X <- sum(pcX$d[1:npcX])/sum(pcX$d); explained_variance_X
+  explained_variance_Y <- sum(pcY$d[1:npcY])/sum(pcY$d); explained_variance_Y
   
   ## SURFACES
   surface_beta_hat <- matrix(0, nrow = lx, ncol = ly)
@@ -78,34 +82,6 @@ fregre.pc.ex <- function(B)
     }
   }
   
-  ## ESTIMATED Y:
-  
-  ## SURFACES
-  y_hat <- Y
-  y_hat$data <- pcY$x %*% t(pcY$rotation)
-  fresiduals <- Y - y_hat
-  res_star <- fresiduals
-  res_star$data <- rwild(fresiduals$data, "golden")
-  
-  par(mfrow = c(1, 4))
-  plot(Y)
-  plot(y_hat)
-  plot(fresiduals)
-  plot(res_star)
-  
-  par(mfrow = c(1, 2))
-  mm <- c(min(as.vector(fresiduals$data)), min(as.vector(res_star$data)))
-  mx <- c(max(as.vector(fresiduals$data)), max(as.vector(res_star$data)))
-  lims <- c(min(mm), max(mx))
-  boxplot(as.vector(fresiduals$data), ylim = lims)
-  boxplot(as.vector(res_star$data), ylim = lims)
-  
-  summary(as.vector(fresiduals$data))
-  summary(as.vector(res_star$data))
-  
-  # Proportion of the residuals that are larger than the bootstraped
-  100*sum(fresiduals$data>res_star$data)/dim(fresiduals$data)[1]/dim(fresiduals$data)[2]
-  
   par(mfrow = c(1, 3))
   minima <- c(min(surface_beta), min(surface_pca), min(surface_beta_hat))
   maxima <- c(max(surface_beta), max(surface_pca), max(surface_beta_hat))
@@ -114,19 +90,29 @@ fregre.pc.ex <- function(B)
   image(ss, tt, surface_pca, col = viridis(20), zlim = limits)
   image(ss, tt, surface_beta_hat, col = viridis(20), zlim = limits)
   
-  #persp(fdataobj$argvals, Y$argvals, surface_beta_hat, phi = 0, theta = 60)
-  #persp(ss, tt, surface_beta, phi = 0, theta = 60)
+  ## RESIDUALS AND BOOTSTRAP
+  y_hat <- Y
+  y_hat$data <- pcY$x %*% t(pcY$rotation)
+  fresiduals <- Y - y_hat$data
+  res_star <- fresiduals; for (j in 1:dim(fresiduals$data)[1]){ #PERTURBED RESIDUALS
+    res_star$data[j,] <- fresiduals$data[j,] * rwild(1, "golden")}
+  Y_star <- y_hat - fresiduals + res_star
   
-  #====================================================================
+  par(mfrow = c(1, 3))
+  mm <- c(min(as.vector(fresiduals$data)), min(as.vector(res_star$data)))
+  mx <- c(max(as.vector(fresiduals$data)), max(as.vector(res_star$data)))
+  lims <- c(min(mm), max(mx))
+  boxplot(as.vector(fresiduals$data), ylim = lims)
+  boxplot(as.vector(res_star$data), ylim = lims)
+  plot(ecdf(as.vector(fresiduals$data)),col=4)
+  lines(ecdf(as.vector(res_star$data)),col=2)
+  legend('topleft', legend=c("Original residuals", "Perturbed residuals"),col=c(3,2), lty=1, cex=0.8)
   
-
-  diferencia <- (sum(((hat_b - theoretical_beta)/theoretical_beta)^2))^(1/(npcX*npcY)); 
+  summary(as.vector(fresiduals$data))
+  summary(as.vector(res_star$data))
   
-  explained_variance_X <- sum(pcX$d[1:npcX])/sum(pcX$d); explained_variance_X
-  explained_variance_Y <- sum(pcY$d[1:npcY])/sum(pcY$d); explained_variance_Y
-  
-  hat_y <- pcX$x %*% hat_b
-  residuals <- pcY$x - hat_y
+  # Proportion of the residuals that are larger than the bootstraped
+  100*sum(fresiduals$data>res_star$data)/dim(fresiduals$data)[1]/dim(fresiduals$data)[2]
   
   ## WILD BOOTSTRAP RESAMPLING
   
@@ -142,25 +128,36 @@ fregre.pc.ex <- function(B)
   Ad=t(Ad)
   Ad=Ad+t(Ad)-diag(diag(Ad))
   
+  residuals <- P %*% Y$data %*% pcY$rotation
   PCvM <- PCvM_statistic(pcX$x, residuals, Ad)
   
-  fresiduals <- Y - y_hat
-  res_star <- fresiduals
   Y_star <- Y
   res_hat_star <- res_star
   
   for (i in 1:B) {
-    res_star$data <- rwild(fresiduals$data, "golden")
+    for (j in 1:dim(fresiduals$data)[1]){
+      res_star$data[j,] <- fresiduals$data[j,] * rwild(1, "golden")
+    }
     Y_star <- y_hat - fresiduals + res_star
-    pcY_star <- fpc(Y_star,npcY,equispaced = TRUE)
-    res_hat_star <- P %*% pcY_star$x
+    pcY_star <- Y_star$data %*% pcY$rotation
+    res_hat_star <- P %*% pcY_star
     PCvM_star[i] = PCvM_statistic(pcX$x, res_hat_star, Ad)
-    print(i)
   }
   
+  par(mfrow = c(1, 1))
   hist(PCvM_star)
   abline(v = PCvM, col = 2)
+  summary(PCvM_star)
+  #print(PCvM)
+  
   pvalue = sum(PCvM_star > PCvM)/B; print(pvalue)
+  
+  par(mfrow = c(2, 3))
+  plot(Y)
+  plot(y_hat)
+  plot(Y_star)
+  plot(fresiduals)
+  plot(res_star)
   
   return(pvalue)
 }
