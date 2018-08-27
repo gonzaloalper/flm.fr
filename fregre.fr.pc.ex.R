60db0419ca230fe5abedeeb3a1a0ed99cc08f082
Gonzalo Álvarez Pérez <38098347+gonzaloalper@users.noreply.github.com>
2018-08-27 18:00:37 +0200

    Update fregre.fr.pc.ex.R

--- fregre.fr.pc.ex.R
+++ fregre.fr.pc.ex.R
@@ -16,7 +16,7 @@ fregre.pc.ex <- function(B)
   tt <- seq(0, 1, l = ly) # Argvals of Y
   
   # Sample covariate
-  n <- 200
+  n <- 100
   X <- rproc2fdata(n = n, ss, sigma="brownian")
   #X <- r.ou(n = n, x0 = seq(-10, 10, l = n), alpha = 2, t = ss)
   X_range <- X$rangeval[2] - X$rangeval[1]
@@ -38,15 +38,26 @@ fregre.pc.ex <- function(B)
   
   Y_range <- Y$rangeval[2] - Y$rangeval[1]
   
+  # Center data. fda.usc's is slower
+  X_cen <- X
+  x_mean <- colMeans(X$data)
+  X_cen$data <- t(t(X$data) - x_mean)
+  
+  Y_cen <- Y
+  y_mean <- colMeans(Y$data)
+  Y_cen$data <- t(t(Y$data) - y_mean)
+  
   # Plots
-  par(mfrow = c(1, 2))
+  par(mfrow = c(2, 2))
   plot(X)
   plot(Y)
+  plot(X_cen)
+  plot(Y_cen)
   
   npcX<-7
   npcY<-7
-  pcX <- fpc(X,npcX,equispaced = TRUE)
-  pcY <- fpc(Y,npcY,equispaced = TRUE)
+  pcX <- fpc(X_cen,npcX,equispaced = TRUE)
+  pcY <- fpc(Y_cen,npcY,equispaced = TRUE)
   
   hat_b_aux <- t(pcX$x[,1:npcX])%*%pcX$x[,1:npcX]
   hat_b_aux <- pseudoinverse(hat_b_aux)
@@ -91,16 +102,16 @@ fregre.pc.ex <- function(B)
   image(ss, tt, surface_beta_hat, col = viridis(20), zlim = limits)
   
   ## RESIDUALS AND BOOTSTRAP
-  y_hat <- Y
-  y_hat$data <- t(t((pcX$x%*%hat_b) %*% t(pcY$rotation)) + colMeans(Y$data))
-  fresiduals <- Y - y_hat
+  y_hat <- Y_cen
+  y_hat$data <- pcX$x %*% hat_b %*% t(pcY$rotation)
+  fresiduals <- Y_cen - y_hat
   par(mfrow = c(1, 3))
-  plot(Y)
+  plot(Y_cen)
   plot(y_hat)
   plot(fresiduals)
   res_star <- fresiduals; for (j in 1:dim(fresiduals$data)[1]){ #PERTURBED RESIDUALS
     res_star$data[j,] <- fresiduals$data[j,] * rwild(1, "golden")}
-  Y_star <- Y - fresiduals + res_star
+  Y_star <- Y_cen - fresiduals + res_star
   
   par(mfrow = c(1, 3))
   mm <- c(min(as.vector(fresiduals$data)), min(as.vector(res_star$data)))
@@ -123,28 +134,28 @@ fregre.pc.ex <- function(B)
   PCvM_star <- 0; 
   
   # Adot
-  Adot.vec=Adot(X)
+  Adot.vec=Adot(X_cen)
   
   # Obtain the entire matrix Adot
-  Ad=diag(rep(Adot.vec[1],dim(X$data)[1]))
+  Ad=diag(rep(Adot.vec[1],dim(X_cen$data)[1]))
   Ad[upper.tri(Ad,diag=FALSE)]=Adot.vec[-1]
   Ad=t(Ad)
   Ad=Ad+t(Ad)-diag(diag(Ad))
 
   PCvM <- PCvM_statistic(pcX$x, fresiduals$data %*% pcY$rotation, Ad)
   
-  Y_star <- Y; y_hat_PC <- Y; res_hat_star <- fresiduals
+  Y_star <- Y_cen; y_hat_PC <- Y_cen; res_hat_star <- fresiduals
   
   for (i in 1:B) {
     for (j in 1:dim(fresiduals$data)[1]){
       res_star$data[j,] <- fresiduals$data[j,] * rwild(1, "golden")
     }
-    Y_star <- Y - fresiduals + res_star
+    Y_star <- Y_cen - fresiduals + res_star
     Y_star_PC <- fpc(Y_star,npcY,equispaced = TRUE)
     
     hat_b_star <- hat_b_aux %*% Y_star_PC$x[,1:npcY]
     
-    y_hat_PC$data <- t(t((pcX$x%*%hat_b_star) %*% t(Y_star_PC$rotation)) + colMeans(Y_star$data))
+    y_hat_PC$data <- pcX$x %*% hat_b_star %*% t(Y_star_PC$rotation)
     
     res_hat_star <- Y_star - y_hat_PC
     PCvM_star[i] = PCvM_statistic(pcX$x, res_hat_star$data %*% Y_star_PC$rotation, Ad)
@@ -159,12 +170,21 @@ fregre.pc.ex <- function(B)
   pvalue = sum(PCvM_star > PCvM)/B; print(pvalue)
   
   par(mfrow = c(2, 3))
-  plot(Y)
+  plot(Y_cen)
   plot(Y_star)
   plot(y_hat_PC)
-  plot(fresiduals)
-  plot(res_star)
-  plot(res_hat_star)
+  
+  fresiduals_plot <- fresiduals
+  fresiduals_plot$data <- fresiduals_plot$data[1:5,]
+  plot(fresiduals_plot,ylim=c(-1,1))
+  
+  res_star_plot <- res_star
+  res_star_plot$data <- res_star_plot$data[1:5,]
+  plot(res_star_plot,ylim=c(-1,1))
+  
+  res_hat_star_plot <- res_hat_star
+  res_hat_star_plot$data <- res_hat_star_plot$data[1:5,]
+  plot(res_hat_star_plot,ylim=c(-1,1))
   
   return(PCvM_star)
 }
