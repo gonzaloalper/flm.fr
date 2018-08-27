4a0c8e4064c2ecd89228cd28fbacabd2a26100fd
Gonzalo Álvarez Pérez <38098347+gonzaloalper@users.noreply.github.com>
2018-08-27 11:41:24 +0200

    Update flm.fourier.R

--- flm.fourier.R
+++ flm.fourier.R
@@ -1,133 +1,91 @@
-fregre.pc.ex <- function(B)
+flm.fourier <- function(B)
 {
-  library(fda.usc)
+  ## FLM WITH FUNCTIONAL RESPONSE EXAMPLE
+  
   library(viridis)
+  library(fda.usc)
+  
   # Execute all the functions in the R directory from the above file:
-  sourceDir <- function(directory = "R", ...) {
-    invisible(sapply(dir(directory),
-                     function(x) source(paste0(directory, "/", x), ...)))
-  }
-  sourceDir()
+  #sourceDir <- function(directory = "R", ...) {
+  #  invisible(sapply(dir(directory),
+  #                   function(x) source(paste0(directory, "/", x), ...)))
+  #}
+  #sourceDir()
   
-  # Argvals
-  lx <- 205
-  ly <- 205
-  ss <- seq(0, 3, l = lx) # Argvals of X
-  tt <- seq(0, 2, l = ly) # Argvals of Y
+  # Grids of X and Y
+  n = 100
+  lx = 201; argvalsx <- seq(0, 1, l = lx) # Argvals of X
+  ly = 201; argvalsy <- seq(0, 1, l = ly) # Argvals of Y
   
-  # Sample covariate
-  n <- 200
-  X <- rproc2fdata(n = n, ss, sigma="brownian")
-  #X <- r.ou(n = n, x0 = seq(-10, 10, l = n), alpha = 2, t = ss)
-  X_range <- X$rangeval[2] - X$rangeval[1]
-  hx <- (X$rangeval[2]-X$rangeval[1])/length(ss)
-  
-  # Theoretical beta
-  beta <- function(s, t) {
-    #diag(2,lx,ly)
-    #2+(s - s + t - t)
-    500 / (1 + (s - 0.5)^2) * cos(2 * pi * t * s) / 200
-    #(s + t^2) / 100
-    #cos(t * s / pi)^2
-  }
-  
-  # Visualization
-  surface_beta <- outer(ss, tt, FUN = beta)
-  
-  Y <- linear_model(X, tt, 0.1, beta)
-  
-  Y_range <- Y$rangeval[2] - Y$rangeval[1]
+  # Data generation
+  data <- data_generation(n = n, ss = argvalsx, tt = argvalsy)
+  X <- data[[1]]; Y <- data[[2]]; beta <- data[[3]]; surface_beta <- data[[4]]
   
   # Plots
   par(mfrow = c(1, 2))
   plot(X)
   plot(Y)
   
-  npcX<-31
-  npcY<-25
-  pcX <- fpc(X,npcX,equispaced = TRUE)
-  pcY <- fpc(Y,npcY,equispaced = TRUE)
+  # Basis expansions
+  kx <- 15; ky <- 15
+  fourier <- fourier_expansion(X, Y, kx, ky)
+  projX <- fourier[[1]]; projY <- fourier[[2]]
+  basis_fourier_x <- fourier[[3]]; basis_fourier_y <- fourier[[4]]
   
-  hat_b <- t(pcX$x[,1:npcX])%*%pcX$x[,1:npcX]
-  hat_b <- pseudoinverse(hat_b)
-  hat_b <- hat_b%*%t(pcX$x[,1:npcX])
-  hat_b <- hat_b%*%pcY$x[,1:npcY]
-  
-  theoretical_beta <- matrix(0, nrow = npcX, ncol = npcY)
-  
-  for (i in 1:npcX){
-    for (j in 1:npcY){
-      outer_product <- outer(pcX$rotation[,i],pcY$rotation[,j])
+  # Coefficients (estimated and theoretical)
+  hat_b <- pseudoinverse(t(projX) %*% projX) %*% t(projX) %*% projY
+  coefs_beta <- matrix(0, nrow = kx, ncol = ky)
+  for (i in 1:kx){
+    for (j in 1:ky){
+      outer_product <- outer(basis_fourier_x[,i],basis_fourier_y[,j])
       acc <- surface_beta * outer_product
-      theoretical_beta[i,j] <- integrateSimp2D(acc, c(X_range, Y_range))
+      coefs_beta[i,j] <- integrateSimp2D(acc, c(X_range, Y_range))
     }
   }
   
-  #====================================================================
+  integrateSimp2D(outer(basis_fourier_x[,i],basis_fourier_y[,j])^2,
+                            c(X_range,Y_range))
   
-  ## SURFACES
-  surface_beta_hat <- matrix(0, nrow = lx, ncol = ly)
-  surface_pca <- matrix(0, nrow = lx, ncol = ly)
-  for (i in 1:npcX){
-    for (j in 1:npcY){
-      outer_product <- outer(pcX$rotation[,i], pcY$rotation[,j])
-      acc1 <- theoretical_beta[i,j] * outer_product
-      surface_pca <- surface_pca + acc1
+  surface_fourier_hat <- matrix(0, nrow = lx, ncol = ly)
+  surface_fourier <- matrix(0, nrow = lx, ncol = ly)
+  
+  for (i in 1:kx){
+    for (j in 1:ky){
+      outer_product <- outer(basis_fourier_x[,i],basis_fourier_y[,j])
+      acc1 <- coefs_beta[i,j] * outer_product
+      surface_fourier <- surface_fourier + acc1
       acc2 <- hat_b[i,j] * outer_product
-      surface_beta_hat <- surface_beta_hat + acc2
+      surface_fourier_hat <- surface_fourier_hat + acc2
     }
   }
-  
-  ## ESTIMATED Y:
-  
-  ## SURFACES
-  y_hat <- Y
-  y_hat$data <- pcY$x %*% t(pcY$rotation)
-  fresiduals <- Y - y_hat
-  res_star <- fresiduals
-  res_star$data <- rwild(fresiduals$data, "golden")
-  
-  par(mfrow = c(1, 4))
-  plot(Y)
-  plot(y_hat)
-  plot(fresiduals)
-  plot(res_star)
-  
-  par(mfrow = c(1, 2))
-  mm <- c(min(as.vector(fresiduals$data)), min(as.vector(res_star$data)))
-  mx <- c(max(as.vector(fresiduals$data)), max(as.vector(res_star$data)))
-  lims <- c(min(mm), max(mx))
-  boxplot(as.vector(fresiduals$data), ylim = lims)
-  boxplot(as.vector(res_star$data), ylim = lims)
-  
-  summary(as.vector(fresiduals$data))
-  summary(as.vector(res_star$data))
   
   par(mfrow = c(1, 3))
-  minima <- c(min(surface_beta), min(surface_pca), min(surface_beta_hat))
-  maxima <- c(max(surface_beta), max(surface_pca), max(surface_beta_hat))
+  minima <- c(min(surface_fourier), min(surface_beta), min(surface_fourier_hat))
+  maxima <- c(max(surface_fourier), max(surface_beta), max(surface_fourier_hat))
   limits = c(min(minima), max(maxima))
   image(ss, tt, surface_beta, col = viridis(20), zlim = limits)
-  image(ss, tt, surface_pca, col = viridis(20), zlim = limits)
-  image(ss, tt, surface_beta_hat, col = viridis(20), zlim = limits)
+  image(ss, tt, surface_fourier, col = viridis(20), zlim = limits)
+  image(ss, tt, surface_fourier_hat, col = viridis(20), zlim = limits)
   
-  #persp(fdataobj$argvals, Y$argvals, surface_beta_hat, phi = 0, theta = 60)
-  #persp(ss, tt, surface_beta, phi = 0, theta = 60)
+  ## RESIDUALS AND BOOTSTRAP
+  y_hat <- Y; y_hat2 <- Y
+  #y_hat$data <- projX %*% hat_b
+  y_hat2$data <- projY%*%t(basis_fourier_y)
+  y_hat$data <- projX %*% hat_b %*% t(basis_fourier_y)
+  par(mfrow = c(1, 4))
+  plot(Y)
+  plot(y_hat2)
+  plot(y_hat)
+  fresiduals <- y_hat - Y
+  plot(fresiduals)
   
-  #====================================================================
+  res_star <- fresiduals; for (j in 1:dim(fresiduals$data)[1]){ #PERTURBED RESIDUALS
+    res_star$data[j,] <- fresiduals$data[j,] * rwild(1, "golden")}
+  Y_star <- Y - fresiduals + res_star
   
-
-  diferencia <- (sum(((hat_b - theoretical_beta)/theoretical_beta)^2))^(1/(npcX*npcY)); 
+  ## WILD BOOTSTRAP   RESAMPLING
   
-  explained_variance_X <- sum(pcX$d[1:npcX])/sum(pcX$d); explained_variance_X
-  explained_variance_Y <- sum(pcY$d[1:npcY])/sum(pcY$d); explained_variance_Y
-  
-  hat_y <- pcX$x %*% hat_b
-  residuals <- pcY$x - hat_y
-  
-  ## WILD BOOTSTRAP RESAMPLING
-  
-  P = diag(1, nrow = n, ncol = n) - pcX$x %*% pseudoinverse(t(pcX$x) %*% pcX$x) %*% t(pcX$x)
+  P = pseudoinverse(t(projX) %*% projX) %*% t(projX)
   PCvM_star <- 0; 
   
   # Adot
@@ -139,25 +97,49 @@ fregre.pc.ex <- function(B)
   Ad=t(Ad)
   Ad=Ad+t(Ad)-diag(diag(Ad))
   
-  PCvM <- PCvM_statistic(pcX$x, residuals, Ad)
+  PCvM <- PCvM_statistic(projX, fresiduals$data %*% basis_fourier_y, Ad)
   
-  fresiduals <- Y - y_hat
   res_star <- fresiduals
-  Y_star <- Y
-  res_hat_star <- res_star
+  
+  Y_star <- Y; y_hat_star <- Y; res_hat_star <- fresiduals
+  projY_star <- projY
   
   for (i in 1:B) {
-    res_star$data <- rwild(fresiduals$data, "golden")
-    Y_star <- y_hat - fresiduals + res_star
-    pcY_star <- fpc(Y_star,npcY,equispaced = TRUE)
-    res_hat_star <- P %*% pcY_star$x
-    PCvM_star[i] = PCvM_statistic(pcX$x, res_hat_star, Ad)
-    print(i)
+    for (j in 1:dim(fresiduals$data)[1]){
+      res_star$data[j,] <- fresiduals$data[j,] * rwild(1, "golden")
+    }
+    
+    Y_star <- Y - fresiduals + res_star
+    
+    for (i in 1:n){
+      for (j in 1:ky){
+        projY_star[i,j] <- integrateSimp1D(Y_star$data[i,]*basis_fourier_y[,j],Y_range)
+      }
+    }
+    
+    hat_b_star <- P %*% projY_star
+    
+    y_hat_star$data <- projX %*% hat_b_star %*% t(basis_fourier_y)
+    
+    res_hat_star <- Y_star - y_hat_star
+    PCvM_star[i] = na.omit(PCvM_statistic(pcX$x, res_hat_star$data %*% basis_fourier_y, Ad))
   }
   
+  PCvM_star <- na.omit(PCvM_star)
+  par(mfrow = c(1,1))
   hist(PCvM_star)
   abline(v = PCvM, col = 2)
-  pvalue = sum(PCvM_star > PCvM)/B; print(pvalue)
+  pvalue = sum(PCvM_star > PCvM)/length(PCvM_star); print(pvalue)
+  
+  par(mfrow = c(2, 4))
+  plot(Y)
+  plot(y_hat)
+  plot(Y_star)
+  plot(y_hat_star)
+  plot(fresiduals)
+  plot(fresiduals)
+  plot(res_star)
+  plot(res_hat_star)
   
   return(pvalue)
 }
